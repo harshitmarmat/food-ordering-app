656d555743a1918cd22333cf53645875556a9288
harshitmarmat <you@example.com>
2021-12-15 01:22:38 +0530

    partially done

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css"
+import mealImage from "../.././assets/meal.jpg"
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props => {
+    return(
+        <Fragment>
+            <header className={classes.header}>
+                <h1>
+                    ReactMeals
+                </h1>
+                <HeaderCartButton/>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealImage} alt="delicious food"/>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header;
---
+++ src/Components/Meals/MealsItem/MealsItem.js
@@ -0,0 +1,20 @@
+import classes from './MealsItem.module.css'
+import MealsItemForm from './MealsItemForm';
+
+const MealsItem = (props) => {
+    const price = `$${props.price}`;
+    return (
+        <li className={classes.meal}>
+            <div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{price}</div>
+            </div>
+            <div>
+                <MealsItemForm id={props.id} />
+            </div>
+        </li>
+    )
+}
+
+export default MealsItem;
